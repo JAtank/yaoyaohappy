3510cf84ce7898eb044e63bb3fbe5284b2b85696
zhoutengteng <zhoutengteng@tbkt.cn>
2017-09-28 18:40:45 +0800

    修改input

--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -5,7 +5,10 @@
     <div class="welcome">
       <div class="up">
         <div class="entering">手动录入</div>
-        <div class="import">文件导入</div>
+        <div class="import">
+          文件导入
+          <input type="file"></input>
+        </div>
       </div>
       <div class="down"></div>
     </div>
@@ -47,11 +50,20 @@
           text-align: center;
         }
         .import{
+          position: relative;
           width: 80px;
           border: 1px solid white;
           border-radius: 15px;
           text-align: center;
           margin-left: 60px;
+          input{
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            position: absolute;
+            left: 0;
+            opacity: 0;
+          }
         }
       }
       .down{
